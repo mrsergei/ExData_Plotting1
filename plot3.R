7ddc68bff7e99f8372fe71b2d51aab0d8e12a2d9
mrsergei <mr.sergei@gmail.com>
2014-09-06 22:10:57 -0700

    caught problem with the box aroudn the legend. fixed it

--- plot3.R
+++ plot3.R
@@ -24,6 +24,6 @@ with(df, plot(datetime, Sub_metering_1, type="l",
      ylab = "Energy sub metering"))
 with(df, lines(datetime, Sub_metering_2, type="l", col="red"))
 with(df, lines(datetime, Sub_metering_3, type="l", col="blue"))
-legend("topright", lty=1, lwd=1, col = c("black", "red", "blue"), 
+legend("topright", lty=1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -12,7 +12,7 @@ df <- read.table("household_power_consumption.txt",
 df <- df[df$Date %in% c("1/2/2007", "2/2/2007"),]
 
 ## convert measurements to the rght class
-df[,3:9] <- lapply(df[,3:9], as.numeric)
+df[3:9] <- lapply(df[3:9], as.numeric)
 
 ## add POSIXlt comliant date and time of the measurements
 df$datetime <- strptime(paste(as.Date(df$Date, "%d/%m/%Y"), df$Time), "%Y-%m-%d %H:%M:%S")
@@ -20,15 +20,15 @@ df$datetime <- strptime(paste(as.Date(df$Date, "%d/%m/%Y"), df$Time), "%Y-%m-%d 
 ## generate plot and save it to the PNG file
 png(file = "plot4.png")
 par(mfcol = c(2, 2))
-with(df, plot(datetime, Global_active_power, type='l',
+with(df, plot(datetime, Global_active_power, type = 'l',
      xlab = "",
      ylab ="Global Active Power (kilowatts)"))
-with(df, plot(datetime, Sub_metering_1, type="l",
+with(df, plot(datetime, Sub_metering_1, type = "l",
      xlab = "",
      ylab = "Energy sub metering"))
-with(df, lines(datetime, Sub_metering_2, type="l", col="red"))
-with(df, lines(datetime, Sub_metering_3, type="l", col="blue"))
-legend("topright", lty=1, lwd=1, col = c("black", "red", "blue"), 
+with(df, lines(datetime, Sub_metering_2, type = "l", col = "red"))
+with(df, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", bty = "n", lty = 1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 with(df, plot(datetime, Voltage, type = "l"))
 with(df, plot(datetime, Global_reactive_power, type = "l"))
